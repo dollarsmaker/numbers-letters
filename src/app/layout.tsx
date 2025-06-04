d197ca319de86b270ad2bdbd3d8cf6fd49b35d7b
dwayneyang <dollarsmaker101@gmail.com>
2025-06-04 13:12:22 +0800

    add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,6 +68,11 @@ export default function RootLayout({
             gtag('config', 'G-M3MZ4B3LQY');
           `}
         </Script>
+        {/*adsense*/}
+        <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9280823005138577"
+                crossOrigin="anonymous">
+        </script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
